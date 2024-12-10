69f2df8605ca98648dfa32e284acb9005e914483
nimraahmed0 <162753499+nimraahmed0@users.noreply.github.com>
2024-12-10 16:11:20 +0500

    Update page.tsx

--- src/app/404/page.tsx
+++ src/app/404/page.tsx
@@ -9,7 +9,7 @@ const Custom404 = () => (
   <Header/>
     <div className="container mx-auto py-16 px-4 bg-black text-white text-center">
       <h1 className="text-5xl font-bold mb-8">404 - Page Not Found</h1>
-      <p className="py-4">The page you're looking for doesn't exist or has been moved.</p>
+      <p className="py-4">The page youre looking for doesnt exist or has been moved.</p>
       <a href="/" className="bg-blue-500 py-2 px-5 mt-8 rounded-md hover:bg-blue-700">
         Go Home
       </a>
